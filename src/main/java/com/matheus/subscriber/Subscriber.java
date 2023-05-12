803e0f998f2110255daef517db33ece0a9c245ea
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-05-12 11:55:07 -0300

    remove: wait for messages in queue. now is reading continuously

--- src/main/java/com/matheus/subscriber/Subscriber.java
+++ src/main/java/com/matheus/subscriber/Subscriber.java
@@ -10,8 +10,6 @@ import java.util.concurrent.TimeoutException;
 public class Subscriber {
 
     private static final String QUEUE_NAME = "NUMBERS_STREAM";
-    private static final int QUEUE_SLEEP_TIME = 2 * 60 * 1000;
-    //private static final int QUEUE_WAIT_TIME = 1000;
 
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         IController controller = IController.createController(Shared.BASIC_ONOFF, 1.0, 100.0);
@@ -33,12 +31,12 @@ public class Subscriber {
             int queueSize = (int) channel.messageCount(QUEUE_NAME);
             if (queueSize > 0) {
                 // Receive a message and ack it
-                GetResponse response = channel.basicGet(QUEUE_NAME, false); // #TO-DO: colocar aqui em espera e dar o ack logo assim que receber
+                GetResponse response = channel.basicGet(QUEUE_NAME, false);
                 if (response != null) {
+                    channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
                     count++;
                     String message = new String(response.getBody(), "UTF-8");
                     System.out.println("Received message: " + message);
-                    channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
                 }
             }
             // Stop timer and update arrival rate
@@ -50,16 +48,10 @@ public class Subscriber {
                 count = 0;
                 // Print queue size and arrival rate
                 System.out.printf("Queue size: %d, Arrival rate: %.2f\n", queueSize, arrivalRate);
-                // #TO-DO: fazer com que a fila esteja sempre cheia de mensagens. Pode ser usando mais de um publisher (100).
-                // if (queueSize < arrivalRate) {
-                //     System.out.println("Queue size is less than arrival rate, sleeping for 2 minutes...");
-                //     Thread.sleep(QUEUE_SLEEP_TIME);
-                // }
                 // Compute new value for prefetch count using controller
                 int newPC = (int) controller.update(queueSize, arrivalRate);
                 System.out.printf("Updated prefetch count: %d\n\n", newPC);
                 // Set new prefetch count
-                // #TO-DO: Analisar o desempenho do sistema com o PC 1 e 10. Verificar se o PC 1 não é muito baixo e o PC 10 não é muito alto.
                 channel.basicQos(newPC);
             }
         }
