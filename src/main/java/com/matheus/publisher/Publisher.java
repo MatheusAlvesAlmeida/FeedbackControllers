17e33c4419282e66962a91f1cd1984c29ae0d6cd
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-05-12 12:12:58 -0300

    feat: make threads for publish messages in queue

--- src/main/java/com/matheus/publisher/Publisher.java
+++ src/main/java/com/matheus/publisher/Publisher.java
@@ -1,30 +1,48 @@
 package com.matheus.publisher;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Publisher {
     private final static String QUEUE_NAME = "NUMBERS_STREAM";
+    private final static int NUM_PUBLISHERS = 100;
+    private final static int DURATION = 15; // in minutes
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            int x = 0;
-            while (true) {
-                String message = "Hello World! " + x;
-                channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-                System.out.println(" [x] Sent '" + message + "'");
-                x++;
 
-                int interval = new Random().nextInt(1000) + 100;
-                Thread.sleep(interval);
-            }
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_PUBLISHERS);
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        for (int i = 0; i < NUM_PUBLISHERS; i++) {
+            executorService.execute(() -> {
+                try (Connection connection = factory.newConnection();
+                        Channel channel = connection.createChannel()) {
+                    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                    int x = 0;
+                    while (true) {
+                        String message = "Hello World! " + x;
+                        channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+                        System.out.println(" [x] Sent '" + message + "'");
+                        x++;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         }
+
+        scheduledExecutorService.schedule(() -> {
+            executorService.shutdownNow();
+            scheduledExecutorService.shutdown();
+        }, DURATION, TimeUnit.MINUTES);
     }
 }
