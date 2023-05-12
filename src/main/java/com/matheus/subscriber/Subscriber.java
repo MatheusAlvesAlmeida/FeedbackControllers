56f3acac67a34a9ac37d857cc6f6f2b18f03fdb0
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-05-12 11:58:31 -0300

    remove: unecessary comments

--- src/main/java/com/matheus/subscriber/Subscriber.java
+++ src/main/java/com/matheus/subscriber/Subscriber.java
@@ -13,13 +13,14 @@ public class Subscriber {
 
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         IController controller = IController.createController(Shared.BASIC_ONOFF, 1.0, 100.0);
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
+        
         Channel channel = connection.createChannel();
-        // Set up the queue
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        // Set up the message rate monitoring loop
+        
         int count = 0;
         double arrivalRate = 0;
         long t1 = 0;
@@ -28,9 +29,9 @@ public class Subscriber {
                 t1 = System.currentTimeMillis();
                 count = 0;
             }
+
             int queueSize = (int) channel.messageCount(QUEUE_NAME);
             if (queueSize > 0) {
-                // Receive a message and ack it
                 GetResponse response = channel.basicGet(QUEUE_NAME, false);
                 if (response != null) {
                     channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
@@ -39,19 +40,17 @@ public class Subscriber {
                     System.out.println("Received message: " + message);
                 }
             }
-            // Stop timer and update arrival rate
+
             long t2 = System.currentTimeMillis();
             if (t2 - t1 >= 1000) {
                 double interval = (t2 - t1) / 1000;
                 arrivalRate = count / interval;
                 t1 = 0;
                 count = 0;
-                // Print queue size and arrival rate
                 System.out.printf("Queue size: %d, Arrival rate: %.2f\n", queueSize, arrivalRate);
                 // Compute new value for prefetch count using controller
                 int newPC = (int) controller.update(queueSize, arrivalRate);
                 System.out.printf("Updated prefetch count: %d\n\n", newPC);
-                // Set new prefetch count
                 channel.basicQos(newPC);
             }
         }
