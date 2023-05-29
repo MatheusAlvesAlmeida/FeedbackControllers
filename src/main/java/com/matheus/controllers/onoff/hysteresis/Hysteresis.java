7942a6987f1c9952e4d71d7a3b963df3cd7417ea
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-05-29 16:22:10 -0300

    fix: hysteresis on off to consider the setpoint

--- src/main/java/com/matheus/controllers/onoff/hysteresis/Hysteresis.java
+++ src/main/java/com/matheus/controllers/onoff/hysteresis/Hysteresis.java
@@ -2,7 +2,6 @@ package com.matheus.controllers.onoff.hysteresis;
 
 import com.matheus.controllers.def.info.Info;
 import com.matheus.controllers.def.ops.IController;
-import com.matheus.shared.Shared;
 
 public class Hysteresis implements IController{
     private Info info;
@@ -13,44 +12,42 @@ public class Hysteresis implements IController{
 
     @Override
     public void initialize(double... params) {
-        if (params.length < 3) {
-            System.out.printf("Error: '%s' controller requires 3 info (min,max,hysteresis band) %n", Shared.HYSTERESIS_ONOFF);
-            System.exit(0);
+        if (params.length < 4) {
+            throw new IllegalArgumentException("Error: 'HysteresisOnOff' controller requires 4 parameters: setpoint, min, max and hysteresis band");
         }
-        this.info.Min = params[0];
-        this.info.Max = params[1];
-        this.info.HysteresisBand = params[2];
+        this.info.SetPoint = params[0];
+        this.info.Min = params[1];
+        this.info.Max = params[2];
+        this.info.HysteresisBand = params[3];
         this.info.PreviousOut = 0.0;
     }
 
     @Override
     public double update(double... input) {
-        double direction = -1.0;
+        double direction = 1.0;
         double u = 0.0;
 
-        double s = input[0];
-        double y = input[1];
+        double s = this.info.SetPoint;
+        double y = input[0];
 
         double err = direction * (s - y);
 
         if (err > -info.HysteresisBand/2.0 && err < info.HysteresisBand/2.0) {
             u = info.PreviousOut; // no action
-        }
-        if (err >= info.HysteresisBand/2.0) {
+        }else if (err >= info.HysteresisBand/2.0) {
             u = info.Max;
-        }
-        if (err <= -info.HysteresisBand/2) {
+        }else if (err <= -info.HysteresisBand/2) {
             u = info.Min;
         }
     
         if (u < info.Min) {
             u = info.Min;
-        }
-        if (u > info.Max) {
+        }else if (u > info.Max) {
             u = info.Max;
         }
 
-        info.PreviousOut = u;
+        this.info.setPreviousOut(u);
+
         return u;
     }
     
