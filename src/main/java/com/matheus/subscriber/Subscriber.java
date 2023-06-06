23d4cccf72f17d6a1fcf22a70a04cf6a8991375e
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-06-06 18:27:07 -0300

    fix: consumer is now updating correctly the PC value

--- src/main/java/com/matheus/subscriber/Subscriber.java
+++ src/main/java/com/matheus/subscriber/Subscriber.java
@@ -1,64 +1,62 @@
 package com.matheus.subscriber;
 
-import com.matheus.controllers.def.ops.IController;
-import com.matheus.shared.Shared;
-import com.matheus.util.SaveOutput;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Subscriber {
 
     private static final String QUEUE_NAME = "NUMBERS";
 
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
-        //IController controller = IController.createController(Shared.BASIC_ONOFF, 1.0, 100.0);
+        int prefetchCount = 0;
 
         ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.basicQos(1000);
 
-        int count = 0;
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.basicQos(0, prefetchCount, true);
+
+        AtomicInteger count = new AtomicInteger(0);
         double arrivalRate = 0;
         long t1 = 0;
+
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                    byte[] body) throws IOException {
+                channel.basicAck(envelope.getDeliveryTag(), false);
+                //System.out.println(" [x] Received '" + new String(body, "UTF-8") + "'");
+                count.incrementAndGet();
+            }
+        };
+
+        String consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
+
         while (true) {
             if (t1 == 0) {
                 t1 = System.currentTimeMillis();
-                count = 0;
-            }
-
-            int queueSize = (int) channel.messageCount(QUEUE_NAME);
-            if (queueSize > 0) {
-                GetResponse response = channel.basicGet(QUEUE_NAME, false);
-                if (response != null) {
-                    channel.basicAck(response.getEnvelope().getDeliveryTag(), false); 
-                    count++;
-                    String message = new String(response.getBody(), "UTF-8");
-                    System.out.println("Received message: " + message);
-                }
-            } else {
-                System.out.println("Queue is empty, stopping subscriber...");
-                break;
+                count.set(0);
             }
 
             long t2 = System.currentTimeMillis();
-            if (t2 - t1 >= 1000) {
-                double interval = (t2 - t1) / 1000;
-                arrivalRate = count / interval;
+            if (t2 - t1 >= 5000) {
+                channel.basicCancel(consumerTag);
+                double interval = (t2 - t1) / 1000.0;
+                arrivalRate = count.get() / interval;
+                System.out.printf("%d, %.2f\n", prefetchCount, arrivalRate);
                 t1 = 0;
-                count = 0;
-                System.out.printf("Queue size: %d, Arrival rate: %.2f\n", queueSize, arrivalRate);
-                // Compute new value for prefetch count using controller
-                //int newPC = (int) controller.update(queueSize, arrivalRate);
-                //System.out.printf("Updated prefetch count: %d\n\n", newPC);
-                //channel.basicQos(newPC);
-                // Save queue size, arrival rate and prefetch count to CSV file
-                SaveOutput.saveToFile("prefetchcount_1000.csv", String.format("%d,%.2f\n", queueSize, arrivalRate));
+                count.set(0);
+                prefetchCount += 10;
+                channel.basicQos(prefetchCount, true);
+                consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
             }
         }
     }
