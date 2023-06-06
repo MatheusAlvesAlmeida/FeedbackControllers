ada34ec6a6a608e6b0b02ffbc220f7c0bd122bd6
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-06-06 18:33:11 -0300

    refact the subscriber code

--- src/main/java/com/matheus/subscriber/Subscriber.java
+++ src/main/java/com/matheus/subscriber/Subscriber.java
@@ -1,7 +1,6 @@
 package com.matheus.subscriber;
 
 import com.rabbitmq.client.*;
-
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,54 +9,74 @@ public class Subscriber {
 
     private static final String QUEUE_NAME = "NUMBERS";
 
-    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
-        int prefetchCount = 0;
-
+    private static ConnectionFactory createConnectionFactory() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUsername("guest");
         factory.setPassword("guest");
         factory.setHost("localhost");
+        return factory;
+    }
 
-        Connection connection = factory.newConnection();
+    private static Channel createChannel(Connection connection) throws IOException {
         Channel channel = connection.createChannel();
-
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.basicQos(0, prefetchCount, true);
+        return channel;
+    }
 
-        AtomicInteger count = new AtomicInteger(0);
-        double arrivalRate = 0;
-        long t1 = 0;
-
-        DefaultConsumer consumer = new DefaultConsumer(channel) {
+    private static DefaultConsumer createConsumer(Channel channel, AtomicInteger count) {
+        return new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
                     byte[] body) throws IOException {
                 channel.basicAck(envelope.getDeliveryTag(), false);
-                //System.out.println(" [x] Received '" + new String(body, "UTF-8") + "'");
+                // System.out.println(" [x] Received '" + new String(body, "UTF-8") + "'");
                 count.incrementAndGet();
             }
         };
+    }
 
-        String consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
+    public static void main(String[] args) {
+        int prefetchCount = 0;
+        AtomicInteger messageCount = new AtomicInteger(0);
+        double arrivalRate = 0;
+        long startTime = 0;
 
-        while (true) {
-            if (t1 == 0) {
-                t1 = System.currentTimeMillis();
-                count.set(0);
+        try {
+            Connection connection = createConnectionFactory().newConnection();
+            try {
+                Channel channel = createChannel(connection);
+                try {
+                    channel.basicQos(0, prefetchCount, true);
+                    DefaultConsumer consumer = createConsumer(channel, messageCount);
+                    String consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
+
+                    while (true) {
+                        if (startTime == 0) {
+                            startTime = System.currentTimeMillis();
+                            messageCount.set(0);
+                        }
+
+                        long currentTime = System.currentTimeMillis();
+                        if (currentTime - startTime >= 5000) {
+                            channel.basicCancel(consumerTag);
+                            double interval = (currentTime - startTime) / 1000.0;
+                            arrivalRate = messageCount.get() / interval;
+                            System.out.printf("%d, %.2f\n", prefetchCount, arrivalRate);
+                            startTime = 0;
+                            messageCount.set(0);
+                            prefetchCount += 10;
+                            channel.basicQos(prefetchCount, true);
+                            consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
+                        }
+                    }
+                } finally {
+                    channel.close();
+                }
+            } finally {
+                connection.close();
             }
-
-            long t2 = System.currentTimeMillis();
-            if (t2 - t1 >= 5000) {
-                channel.basicCancel(consumerTag);
-                double interval = (t2 - t1) / 1000.0;
-                arrivalRate = count.get() / interval;
-                System.out.printf("%d, %.2f\n", prefetchCount, arrivalRate);
-                t1 = 0;
-                count.set(0);
-                prefetchCount += 10;
-                channel.basicQos(prefetchCount, true);
-                consumerTag = channel.basicConsume(QUEUE_NAME, false, consumer);
-            }
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
         }
     }
 }
