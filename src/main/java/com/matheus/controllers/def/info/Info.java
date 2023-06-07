42ee95b5c337e60294ba09821b9a9f5ad17851b2
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-06-07 17:24:55 -0300

    adjust names of the variables to java pattern

--- src/main/java/com/matheus/controllers/def/info/Info.java
+++ src/main/java/com/matheus/controllers/def/info/Info.java
@@ -1,55 +1,55 @@
 package com.matheus.controllers.def.info;
 
 public class Info {
+    public String typeName; // Controller type name
+    public double setPoint; // Set point
+    public double kp; // kp constant used by PID controllers
+    public double ki; // ki constant used by PID controllers
+    public double kd; // kd constant used by PID controllers
 
-    public String TypeName; // Controller type name
-    public double SetPoint; // Set point
-    public double Kp; // kp constant used by PID controllers
-    public double Ki; // ki constant used by PID controllers
-    public double Kd; // kd constant used by PID controllers
+    public double min; // Minimum value of the controller output
+    public double max; // Maximum value of the controller output
 
-    public double Min; // Minimum value of the controller output
-    public double Max; // Maximum value of the controller output
-
-    public double Integrator; // Integrator component
-    public double SumPrevErrors; // Sum of previous errors -- used by some controllers
-    public double PreviousOut; // Previous output -- used by some controllers
-    public double PreviousError; // Last error -- used by some controllers
-    public double PreviousPreviousError; // Penultimate error -- used by some controllers
-    public double PreviousDifferentiator; // Antepenultimate error -- used by some controllers
-    public double DeadZone; // Dead zone band used by some controllers
-    public double HysteresisBand; // Hysteresis band used by some controllers
-    public double Out; // Controller output
+    public double integrator; // Integrator component
+    public double sumPrevErrors; // Sum of previous errors -- used by some controllers
+    public double previousOut; // Previous output -- used by some controllers
+    public double previousError; // Last error -- used by some controllers
+    public double previousPreviousError; // Penultimate error -- used by some controllers
+    public double previousDifferentiator; // Antepenultimate error -- used by some controllers
+    public double deadZone; // Dead zone band used by some controllers
+    public double hysteresisBand; // Hysteresis band used by some controllers
+    public double out; // Controller output
 
     public void setType(String basicOnoff) {
-        this.TypeName = basicOnoff;
+        this.typeName = basicOnoff;
     }
 
     public void setMin(double min2) {
-        this.Min = min2;
+        this.min = min2;
     }
 
     public void setMax(double max2) {
-        this.Max = max2;
+        this.max = max2;
     }
 
     public double getMax() {
-        return this.Max;
+        return this.max;
     }
 
     public double getMin() {
-        return this.Min;
+        return this.min;
     }
 
     public void setSetPoint(double setpoint) {
-        this.SetPoint = setpoint;
+        this.setPoint = setpoint;
     }
 
     public double getSetPoint() {
-        return this.SetPoint;
+        return this.setPoint;
     }
 
     public void setPreviousOut(double previousOut) {
-        this.PreviousOut = previousOut;
+        this.previousOut = previousOut;
     }
+
 }
